972f4c0359eb9ba12487223c518588e3be1689d3
finnformica <finnformica@gmail.com>
2023-06-02 14:40:27 +0100

    refactor navbar menu button and links

---
+++ src/components/Header/NavMenu.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+import { Box, Menu, MenuItem, Typography, IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+import { pages } from "../../constants";
+
+const NavMenu = () => {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <Box sx={{ display: { xs: "flex", md: "none" } }}>
+      <IconButton
+        size="large"
+        aria-label="account of current user"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        onClick={handleOpenNavMenu}
+        color="inherit"
+      >
+        <MenuIcon />
+      </IconButton>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorElNav}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        open={Boolean(anchorElNav)}
+        onClose={handleCloseNavMenu}
+        sx={{
+          display: { xs: "block", md: "none" },
+        }}
+      >
+        {pages.map((page, key) => (
+          <MenuItem key={key} onClick={handleCloseNavMenu}>
+            <Typography textAlign="center">{page.name}</Typography>
+          </MenuItem>
+        ))}
+      </Menu>
+    </Box>
+  );
+};
+
+export default NavMenu;
