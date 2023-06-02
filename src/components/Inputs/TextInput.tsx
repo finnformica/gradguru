558307f99a82cff22000653471b32e6f593a9589
finnformica <finnformica@gmail.com>
2023-06-02 17:25:44 +0100

    add focus state to text input

--- src/components/Inputs/TextInput.tsx
+++ src/components/Inputs/TextInput.tsx
@@ -1,8 +1,17 @@
+import React, { useState } from "react";
+import { useTheme } from "@mui/material";
+
 const TextInput = ({ placeholder, ...props }: any) => {
+  const [hasFocus, setFocus] = useState(false);
+
+  const theme = useTheme();
+
   return (
     <input
       type="text"
       placeholder={placeholder}
+      onFocus={() => setFocus(true)}
+      onBlur={() => setFocus(false)}
       style={{
         height: "32px",
         borderRadius: "8px 2px 2px 8px",
@@ -10,7 +19,8 @@ const TextInput = ({ placeholder, ...props }: any) => {
         paddingLeft: "16px",
         marginRight: "6px",
         backgroundColor: "#EEF2F2",
-        width: "250px",
+        width: "275px",
+        outline: hasFocus ? `2px solid ${theme.palette.primary.main}` : "none",
         ...props,
       }}
     />
