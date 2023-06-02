9b43e898fe6663f8c2083d9568d6aaf64864e140
finnformica <finnformica@gmail.com>
2023-06-02 17:25:56 +0100

    add company logos to section

--- src/components/Companies/CompanyLogos.tsx
+++ src/components/Companies/CompanyLogos.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Container, Grid } from "@mui/material";
+
+import { companies } from "@/constants";
+import Image from "next/image";
 
 const CompanyLogos = () => {
-  return <div>CompanyLogos</div>;
+  return (
+    <Container maxWidth="lg" sx={{ py: 6 }}>
+      <Grid container spacing={0}>
+        {companies.map((item, key) => (
+          <Grid key={key} item xs={4} md={2}>
+            <Image
+              width={100}
+              height={100}
+              key={key}
+              src={item.src}
+              alt={`${item.name} logo`}
+              style={{ objectFit: "contain" }}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 };
 
 export default CompanyLogos;
