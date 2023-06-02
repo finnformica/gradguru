9118f916c03d60280a22d3d86292a60e91e3c2cf
finnformica <finnformica@gmail.com>
2023-06-02 14:40:43 +0100

    make navbar responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,34 +1,27 @@
 "use client";
 
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Box,
-  Container,
-  useTheme,
-} from "@mui/material";
+import { AppBar, Toolbar, Box, Container, useTheme } from "@mui/material";
 
 import SquareButton from "../Buttons/SquareButton";
-import NavLink from "./NavLink";
-
-import { pages } from "../../constants";
+import NavLinks from "./NavLinks";
+import NavbarLogo from "./NavbarLogo";
+import NavMenu from "./NavMenu";
 
 const Header = () => {
   const theme = useTheme();
 
   return (
-    <Container maxWidth="xl">
-      <AppBar
-        position="static"
-        sx={{
-          background: "transparent",
-          boxShadow: "none",
-          py: 2,
-          m: 0,
-        }}
-      >
-        <Toolbar sx={{ backgroundColor: "#FFF", px: 0 }}>
+    <AppBar
+      position="static"
+      sx={{
+        background: "transparent",
+        boxShadow: "none",
+        py: 2,
+        m: 0,
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar sx={{ backgroundColor: "#FFF" }} disableGutters>
           <Box
             sx={{
               flexGrow: 1,
@@ -36,28 +29,15 @@ const Header = () => {
               alignItems: "center",
             }}
           >
-            <Typography
-              variant="h3"
-              fontWeight={400}
-              sx={{ color: theme.palette.primary.main, letterSpacing: 2 }}
-            >
-              gradguru
-            </Typography>
+            <NavMenu />
+            <NavbarLogo />
 
-            <Box sx={{ display: "flex", flexDirection: "row", gap: 4, pl: 4 }}>
-              {pages.map((page, key: number) => (
-                <NavLink href={page.route} key={key}>
-                  <Typography variant="body1" fontWeight={500}>
-                    {page.name}
-                  </Typography>
-                </NavLink>
-              ))}
-            </Box>
+            <NavLinks />
           </Box>
           <SquareButton>Log in</SquareButton>
         </Toolbar>
-      </AppBar>
-    </Container>
+      </Container>
+    </AppBar>
   );
 };
 
