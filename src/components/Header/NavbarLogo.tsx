e9e97accc9850e769c9724b58b90fd905b9190b4
finnformica <finnformica@gmail.com>
2023-06-02 14:40:02 +0100

    refactor navbar logo

---
+++ src/components/Header/NavbarLogo.tsx
@@ -0,0 +1,20 @@
+import { Typography, useTheme } from "@mui/material";
+
+const NavbarLogo = () => {
+  const theme = useTheme();
+  return (
+    <Typography
+      variant="h3"
+      fontWeight={400}
+      sx={{
+        color: theme.palette.primary.main,
+        letterSpacing: 2,
+        mx: { xs: "auto", md: 0 },
+      }}
+    >
+      gradguru
+    </Typography>
+  );
+};
+
+export default NavbarLogo;
