492ec8e474569169e9c009ffd404cb66f816538b
finnformica <finnformica@gmail.com>
2023-06-03 19:24:26 +0100

    add favicon to site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,29 @@ import GradguruThemeProvider from "@/theme";
 export const metadata = {
   title: "gradguru",
   description: "Coaching platform for graduate students.",
+  icons: [
+    {
+      rel: "favicon",
+      url: "/favicon/favicon.ico",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon/favicon-16x16.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/favicon/apple-touch-icon.png",
+    },
+  ],
 };
 
 export default function RootLayout({
