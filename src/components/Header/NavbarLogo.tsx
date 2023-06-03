4da9eb0dca1c733d52324a97b061e854d5488bdd
finnformica <finnformica@gmail.com>
2023-06-03 19:26:46 +0100

    minor cosmetic changes

--- src/components/Header/NavbarLogo.tsx
+++ src/components/Header/NavbarLogo.tsx
@@ -9,7 +9,11 @@ const NavbarLogo = () => {
     <Image
       src="/big-logo.png"
       alt="Graduate coaching logo"
-      style={{ objectFit: "cover", margin: isMobile ? "0 auto" : "0" }}
+      style={{
+        objectFit: "cover",
+        margin: isMobile ? "0 auto" : "0",
+        paddingRight: isMobile ? "0" : "1rem",
+      }}
       width={310}
       height={70}
       priority
