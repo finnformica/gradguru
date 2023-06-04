7966a2dd3e377ebc1248ebe59933bce4b65f4062
finnformica <finnformica@gmail.com>
2023-06-04 15:43:21 +0100

    hirevue component

---
+++ src/components/Hirevue/index.tsx
@@ -0,0 +1,38 @@
+import { Box, Container } from "@mui/material";
+import SmallTitle from "../Titles/SmallTitle";
+
+import ItemCard from "./ItemCard";
+import cards from "./cards";
+
+const Hirevue = () => {
+  return (
+    <Container maxWidth="xl">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          py: { xs: 2, md: 6 },
+        }}
+      >
+        <SmallTitle sx={{ textAlign: "center", pb: 4 }}>
+          Practice drills for every stage of the process
+        </SmallTitle>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            justifyContent: "center",
+            gap: { xs: 4, md: 2, lg: 8 },
+            alignItems: "center",
+          }}
+        >
+          {cards.map((item, key) => (
+            <ItemCard key={key} {...item} />
+          ))}
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Hirevue;
