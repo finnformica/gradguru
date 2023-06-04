e8ce64cdae6638e2542f1ee04e2fb69efbdab3f7
finnformica <finnformica@gmail.com>
2023-06-04 17:36:07 +0100

    add link to gradguru logo

--- src/components/Header/NavbarLogo.tsx
+++ src/components/Header/NavbarLogo.tsx
@@ -1,23 +1,30 @@
 import { useTheme, useMediaQuery } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
 
 const NavbarLogo = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   return (
-    <Image
-      src="/big-logo.png"
-      alt="Graduate coaching logo"
+    <Link
+      href="/"
       style={{
-        objectFit: "cover",
         margin: isMobile ? "0 auto" : "0",
         paddingRight: isMobile ? "0" : "1rem",
       }}
-      width={310}
-      height={70}
-      priority
-    />
+    >
+      <Image
+        src="/big-logo.png"
+        alt="Graduate coaching logo"
+        style={{
+          objectFit: "cover",
+        }}
+        width={310}
+        height={70}
+        priority
+      />
+    </Link>
   );
 };
 
