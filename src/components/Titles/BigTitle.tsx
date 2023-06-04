d231a406f9ddbb851438e275caf489bfbd8169b5
finnformica <finnformica@gmail.com>
2023-06-04 14:36:47 +0100

    pass sx props into BigTitle

--- src/components/Titles/BigTitle.tsx
+++ src/components/Titles/BigTitle.tsx
@@ -1,13 +1,14 @@
 import { Typography } from "@mui/material";
+
 import { TitleProps } from "../globalTypes";
 
-const BigTitle = ({ children }: TitleProps) => {
+const BigTitle = ({ children, ...props }: TitleProps) => {
   return (
     <Typography
       variant="h1"
       fontSize="2.25rem"
       fontWeight={500}
-      sx={{ pb: 2, letterSpacing: "1px" }}
+      sx={{ pb: 2, letterSpacing: "1px", ...props.sx }}
     >
       {children}
     </Typography>
