b7785f2c0168870f0ec6b19bd13f6efabe11104f
finnformica <finnformica@gmail.com>
2023-06-11 19:23:27 +0100

    add welcome email obj

--- firebase/utils.ts
+++ firebase/utils.ts
@@ -1,4 +1,10 @@
-import { getFirestore, doc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  collection,
+  setDoc,
+  addDoc,
+} from "firebase/firestore";
 import { app } from "./config";
 
 const db = getFirestore(app);
@@ -6,21 +12,46 @@ const db = getFirestore(app);
 const addData = async (
   data: FirestoreDataType,
   id: string | null,
-  collection: FirestoreCollectionType
+  collectionName: FirestoreCollectionType
 ) => {
   let error,
     result = null;
   try {
-    result = await setDoc(
-      id ? doc(db, collection, id) : doc(db, collection),
-      data,
-      { merge: true }
-    );
+    console.log("writing to db...");
+
+    result = id
+      ? await setDoc(doc(db, collectionName, id), data, { merge: true })
+      : await addDoc(collection(db, collectionName), data);
+
+    console.log("write successful");
   } catch (e) {
     error = e;
+    console.log("error writing to db", e);
   }
 
   return { result, error };
 };
 
-export { addData };
+const welcomeEmailConfig = {
+  subject: "Welcome to Gradguru",
+  text: `
+      Welcome! 
+
+      Thank you for signing up to Gradguru - we are excited to have you on board. Gradguru is a platform that helps you find the right graduate program for you. We do this by providing you with the tools to find the right program, the right school, and the right funding. Our goal is to help you find the right program for you, and to help you get into that program.
+      Gradguru is currently in beta, and we are working hard to make it the best platform for you. Thank you for joining us on this journey.
+
+      Best,
+      The Gradguru Team
+  `,
+  html: `
+    <h1>Welcome!</h1>
+
+    <p>Thank you for signing up to Gradguru - we are excited to have you on board. Gradguru is a platform that helps you find the right graduate program for you. We do this by providing you with the tools to find the right program, the right school, and the right funding. Our goal is to help you find the right program for you, and to help you get into that program.</p>
+    <p>Gradguru is currently in beta, and we are working hard to make it the best platform for you. Thank you for joining us on this journey.</p>
+    
+    <p>Best,</p>
+    <p>The Gradguru Team</p>
+  `,
+};
+
+export { addData, welcomeEmailConfig };
