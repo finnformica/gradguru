fa6bb68e8b7449cd355946bbc5337bb54fd0a483
finnformica <finnformica@gmail.com>
2023-08-31 19:01:29 +0700

    remove link styles globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+import "@/styles/global.css";
+
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
