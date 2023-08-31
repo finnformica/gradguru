aed6de260ef3781b56f756e82cc865c021059d16
finnformica <finnformica@gmail.com>
2023-08-31 18:13:33 +0700

    email sign up with ConvertKit

---
+++ src/app/api/convertkit/subscribe/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const API_KEY = process.env.CONVERTKIT_API_KEY;
+const FORM_ID = process.env.CONVERTKIT_SUBSCRIBE_FORM_ID;
+const TAG_ID = process.env.CONVERTKIT_SUBSCRIBE_TAG_ID;
+const BASE_URL = "https://api.convertkit.com/v3/";
+
+export async function POST(request: NextRequest) {
+  const { email } = await request.json();
+
+  const response = await fetch(`${BASE_URL}/forms/${FORM_ID}/subscribe`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      api_key: API_KEY,
+      email,
+      tags: [TAG_ID],
+    }),
+  });
+
+  if (response.ok) {
+    return NextResponse.json({
+      message: "Success",
+      status: 200,
+    });
+  } else {
+    return NextResponse.json({
+      message: "Error",
+      status: 400,
+    });
+  }
+}
--- src/utils/subscribe.ts
+++ src/utils/subscribe.ts
@@ -1,18 +1,24 @@
-import { addData } from "../../firebase/utils";
-import { AlertState } from "../components/globalTypes";
+import { AlertState } from "@/components/globalTypes";
 
 const subscribe = async (
   email: string,
   setState: (state: AlertState) => void,
   state: AlertState
 ) => {
-  const { result, error } = await addData(
-    { email, subscribed: true },
-    email,
-    "mail-list"
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/convertkit/subscribe`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+      }),
+    }
   );
 
-  if (error) {
+  if (response.status !== 200) {
     setState({
       ...state,
       open: true,
@@ -25,41 +31,10 @@ const subscribe = async (
       ...state,
       open: true,
       severity: "success",
-      message: "Subscription successful!",
+      message: "Please check your email for a confirmation link.",
       title: "Success!",
     });
   }
-
-  await addData(
-    {
-      to: email,
-      message: welcomeEmailConfig,
-    },
-    null,
-    "mail"
-  );
-};
-
-const welcomeEmailConfig = {
-  subject: "Welcome to Gradguru",
-  text: `
-        Welcome! 
-  
-        Thank you for signing up to Gradguru - we are excited to have you on board. Gradguru is a platform that helps you find the right graduate program for you. We do this by providing you with the tools to find the right program, the right school, and the right funding. Our goal is to help you find the right program for you, and to help you get into that program.
-        Gradguru is currently in beta, and we are working hard to make it the best platform for you. Thank you for joining us on this journey.
-  
-        Best,
-        The Gradguru Team
-    `,
-  html: `
-      <h1>Welcome!</h1>
-  
-      <p>Thank you for signing up to Gradguru - we are excited to have you on board. Gradguru is a platform that helps you find the right graduate program for you. We do this by providing you with the tools to find the right program, the right school, and the right funding. Our goal is to help you find the right program for you, and to help you get into that program.</p>
-      <p>Gradguru is currently in beta, and we are working hard to make it the best platform for you. Thank you for joining us on this journey.</p>
-      
-      <p>Best,</p>
-      <p>The Gradguru Team</p>
-    `,
 };
 
 export { subscribe };
