8c7340a127a77d5973f7dea827daf156ff3bf456
finnformica <finnformica@gmail.com>
2023-09-01 15:33:05 +0700

    refactor icon locations

--- src/components/HomePage/Features.tsx
+++ src/components/HomePage/Features.tsx
@@ -12,19 +12,19 @@ const features = [
     title: "Video Courses",
     description:
       "We'll walk you step by step through the application process of the company of your choice, and teach you what  the winning formula entails.",
-    src: "icons/video.svg",
+    src: "icons/feature-icons/video.svg",
   },
   {
     title: "Practice Drills",
     description:
       "Practice drills relevant to your application process so that you're ready for when the application time comes.",
-    src: "icons/test.svg",
+    src: "icons/feature-icons/test.svg",
   },
   {
     title: "Independent Practice Material",
     description:
       "Practice materials for you to use in your own time and to give you a competitive advantage.",
-    src: "icons/head.svg",
+    src: "icons/feature-icons/head.svg",
   },
 ];
 
