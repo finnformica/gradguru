3540354d21f0ece6bb8daff85717195e7cc47d5c
finnformica <finnformica@gmail.com>
2023-09-01 15:36:19 +0700

    refactor company logos

--- src/components/HomePage/Companies.tsx
+++ src/components/HomePage/Companies.tsx
@@ -8,27 +8,27 @@ import FullWidthContainer from "../Containers/FullWidthContainer";
 const companies = [
   {
     name: "Deloitte",
-    src: "/imgs/logos/logo1.png",
+    src: "/imgs/company-logos/logo1.png",
   },
   {
     name: "PwC",
-    src: "/imgs/logos/logo2.png",
+    src: "/imgs/company-logos/logo2.png",
   },
   {
     name: "KPMG",
-    src: "/imgs/logos/logo3.png",
+    src: "/imgs/company-logos/logo3.png",
   },
   {
     name: "EY",
-    src: "/imgs/logos/logo4.png",
+    src: "/imgs/company-logos/logo4.png",
   },
   {
     name: "JP Morgan",
-    src: "/imgs/logos/logo5.png",
+    src: "/imgs/company-logos/logo5.png",
   },
   {
     name: "BAE Systems",
-    src: "/imgs/logos/logo6.png",
+    src: "/imgs/company-logos/logo6.png",
   },
 ];
 
