a048cfc060013f5047c2476d33745a7b0d01f6c2
finnformica <finnformica@gmail.com>
2023-09-16 17:50:26 +0700

    add video element

--- src/app/(app)/courses/[course]/page.tsx
+++ src/app/(app)/courses/[course]/page.tsx
@@ -2,8 +2,11 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { Box } from "@mui/material";
 
 import { useAuth } from "@/context/auth";
+
+import { consultingCourse } from "@/mock/courses";
 
 type CoursePageProps = {
   params: { course: string };
@@ -13,20 +16,39 @@ const CoursePage = ({ params: { course } }: CoursePageProps) => {
   const { user, loading } = useAuth();
   const router = useRouter();
 
-  // useEffect(() => {
-  //   if (!user && !loading) {
-  //     router.push("/login");
-  //   }
-  // }, [user, loading, router]);
+  useEffect(() => {
+    if (!user && !loading) {
+      router.push("/login");
+    }
+  }, [user, loading, router]);
 
-  // if (!user) {
-  //   return null;
-  // }
+  if (!user) {
+    return null;
+  }
 
   return (
-    <>
-      <div>Course: {course}</div>
-    </>
+    <Box
+      sx={{
+        // backgroundColor: (theme) => theme.palette.grey[100],
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <video
+        controls
+        width="100%"
+        title={`${consultingCourse.sections[0].lessons[0].name} - ${consultingCourse.sections[0].name} - ${consultingCourse.name}`}
+        style={{
+          aspectRatio: "16/9",
+          border: "none",
+          maxWidth: "1200px",
+          margin: "0 auto",
+        }}
+      >
+        <source src="/welcome-vid.mp4" type="video/mp4" />
+      </video>
+    </Box>
   );
 };
 
