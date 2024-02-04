1d57bd86d44af5a68cdacb093190edeaada7fe15
finnformica <finnformica@gmail.com>
2024-02-04 20:15:12 +0000

    add comments and remove id field if exists

--- src/app/api/firebase/document/route.ts
+++ src/app/api/firebase/document/route.ts
@@ -7,25 +7,30 @@ import {
   retrieveDocument,
 } from "@/firebase/utils";
 
+// READ
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.nextUrl);
   const collection = searchParams.get("collection") as FirestoreCollectionType;
   const document = searchParams.get("document");
 
   if (!collection) {
+    // if no collection is provided, throw an error
     return NextResponse.json(
       { message: "No collection provided" },
       { status: 400 }
     );
   } else if (!document) {
+    // if no document is provided, retrieve all documents
     const documents = await retrieveAllDocuments(collection);
     return NextResponse.json({ documents }, { status: 200 });
   } else {
+    // if document is provided, retrieve that document
     const data = await retrieveDocument(collection, document);
     return NextResponse.json({ data }, { status: 200 });
   }
 }
 
+// CREATE or UPDATE (for update, provide document id)
 export async function POST(request: NextRequest) {
   const { searchParams } = new URL(request.nextUrl);
   const collection = searchParams.get("collection") as FirestoreCollectionType;
@@ -33,6 +38,7 @@ export async function POST(request: NextRequest) {
 
   const data = await request.json();
 
+  // if no collection is provided, throw an error
   if (!collection) {
     return NextResponse.json(
       { message: "No collection provided" },
@@ -40,7 +46,8 @@ export async function POST(request: NextRequest) {
     );
   }
 
-  const updatedData = { ...data, created: Date.now() };
+  const updatedData = { ...data, created: Date.now() }; // add created timestamp
+  delete updatedData.id; // remove id if it exists
 
   const { result, error } = await addData(updatedData, document, collection);
 
