7c0dbfb33312b3bb42e1d07713a9c7444dfb18ec
finnformica <finnformica@gmail.com>
2024-02-04 15:55:35 +0000

    refactor initial states of sjt question

--- src/app/(admin)/admin/tests/sjt/types.ts
+++ src/app/(admin)/admin/tests/sjt/types.ts
@@ -1,13 +1,26 @@
-export type QuestionState = {
+export interface SJTQuestionState {
   type: "rank" | "multiple";
   question: string;
   options: string[];
   explanation: string;
   answer: string;
+}
+
+export interface SJTScenarioState {
+  scenario: string;
+  questions: SJTQuestionState[];
+  created?: number;
+}
+
+export const initialQuestion = {
+  type: "multiple" as "rank" | "multiple",
+  question: "",
+  options: ["", "", "", "", ""],
+  explanation: "",
+  answer: "1",
 };
 
-export type ScenarioState = {
-  scenario: string;
-  questions: QuestionState[];
-  created: number;
+export const initialForm = {
+  scenario: "",
+  questions: [initialQuestion],
 };
