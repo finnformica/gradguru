4d0672f5bc6754f9a0c3abf054791d57123683bf
finnformica <finnformica@gmail.com>
2024-02-04 14:55:10 +0000

    render documents in list

--- src/app/(admin)/admin/tests/sjt/all/page.tsx
+++ src/app/(admin)/admin/tests/sjt/all/page.tsx
@@ -1,7 +1,86 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemButton,
+  Divider,
+  Stack,
+  Box,
+} from "@mui/material";
+
+import { ScenarioState } from "../types";
 
 const AllSJT = () => {
-  return <div>AllSJT</div>;
+  const [questions, setQuestions] = useState<ScenarioState[]>([]);
+
+  const sortDesc = () => {
+    setQuestions(questions.sort((a, b) => b.created - a.created));
+  };
+
+  const sortAsc = () => {
+    setQuestions(questions.sort((a, b) => a.created - b.created));
+  };
+
+  useEffect(() => {
+    const fetchSJT = async () => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/firebase/document?collection=sjt-consulting`
+      );
+      const data = await response.json();
+
+      setQuestions(
+        data.documents.sort(
+          (a: ScenarioState, b: ScenarioState) => b.created - a.created
+        )
+      );
+    };
+    fetchSJT();
+  }, []);
+
+  return (
+    <>
+      <Typography variant="h4" pb={2}>
+        All SJT questions
+      </Typography>
+      <List>
+        {questions.map((question, key) => (
+          <>
+            <ListItem key={key} disablePadding>
+              <ListItemButton>
+                <Box
+                  sx={{
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      overflow: "hidden",
+                      whiteSpace: "nowrap",
+                      textOverflow: "ellipsis",
+                      width: "60vw",
+                    }}
+                  >
+                    {question.scenario.length === 0
+                      ? "No scenario provided"
+                      : question.scenario}
+                  </Typography>
+                  <Typography>
+                    {new Date(question.created).toLocaleDateString()}
+                  </Typography>
+                </Box>
+              </ListItemButton>
+            </ListItem>
+            <Divider />
+          </>
+        ))}
+      </List>
+    </>
+  );
 };
 
 export default AllSJT;
