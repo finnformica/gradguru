a28b8957f3b62ce8e7351a05f0922f7a770a153c
finnformica <finnformica@gmail.com>
2024-02-08 15:03:21 +0000

    create line chart

--- src/components/Charts/LineChart.tsx
+++ src/components/Charts/LineChart.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
 
-const LineChart = () => {
-  return <div>LineChart</div>;
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { Line } from "react-chartjs-2";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+import { IGraphForm } from "@/components/NRForm/types";
+import { buildChartData } from "./utils";
+
+Chart.register(CategoryScale, ChartDataLabels);
+
+type LineChartProps = {
+  data: IGraphForm["data"];
+};
+
+const LineChart = ({ data }: LineChartProps) => {
+  if (data.rows.length === 0) {
+    return <>No data</>;
+  }
+
+  const [chartData, setChartData] = useState(buildChartData(data));
+
+  useEffect(() => {
+    setChartData(buildChartData(data));
+  }, [data]);
+
+  return (
+    <Line
+      data={chartData}
+      options={{
+        plugins: {
+          datalabels: {
+            display: true,
+            color: "black",
+            align: "top",
+          },
+          title: {
+            display: true,
+            text: data?.labels?.title || "",
+          },
+        },
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: data?.labels?.x || "",
+            },
+          },
+          y: {
+            title: {
+              display: true,
+              text: data?.labels?.y || "",
+            },
+          },
+        },
+      }}
+    />
+  );
 };
 
 export default LineChart;
