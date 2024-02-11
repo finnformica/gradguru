d876c92442db158bc0c614363ef4cf69af52ef3b
finnformica <finnformica@gmail.com>
2024-02-11 19:01:49 +0000

    refactor code

--- src/components/NRForm/GmatForm.tsx
+++ src/components/NRForm/GmatForm.tsx
@@ -16,78 +16,87 @@ type GmatFormProps = {
   setForm: (form: IGmatForm) => void;
 };
 
+const InputContainer = ({ children }: { children: React.ReactNode }) => (
+  <Stack spacing={1}>{children}</Stack>
+);
+
 const GmatForm = ({ form, setForm }: GmatFormProps) => {
   return (
-    <>
-      <Typography variant="h5" pt={2} pb={1}>
-        Question
-      </Typography>
-      <TextField
-        label="Question"
-        fullWidth
-        multiline
-        value={form.question}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-          setForm({ ...form, question: event.target.value })
-        }
-      />
-      <Typography variant="h5" pt={2} pb={1}>
-        Explanation
-      </Typography>
-      <TextField
-        label="Explanation"
-        fullWidth
-        multiline
-        value={form.explanation}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-          setForm({ ...form, explanation: event.target.value })
-        }
-      />
-      <Box pt={2}>
-        <InputLabel sx={{ pb: 1 }}>Explanation preview</InputLabel>
+    <Stack spacing={2} pt={2}>
+      <InputContainer>
+        <Typography variant="h5">Question</Typography>
+        <TextField
+          label="Question"
+          fullWidth
+          multiline
+          required
+          value={form.question}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setForm({ ...form, question: event.target.value })
+          }
+        />
+      </InputContainer>
+      <InputContainer>
+        <Typography variant="h5">Explanation</Typography>
+        <TextField
+          label="Explanation"
+          fullWidth
+          multiline
+          required
+          value={form.explanation}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setForm({ ...form, explanation: event.target.value })
+          }
+        />
+      </InputContainer>
+      <InputContainer>
+        <InputLabel>Explanation preview</InputLabel>
         <ClientWrapper>
           <MathJax hideUntilTypeset={"first"}>{form.explanation}</MathJax>
         </ClientWrapper>
-      </Box>
+      </InputContainer>
 
-      <Typography variant="h5" pt={2} pb={1}>
-        Answer
-      </Typography>
-      <Stack direction="row" spacing={2}>
-        <TextField
-          label="Answer"
-          fullWidth
-          multiline
-          value={form.answer.value}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            setForm({
-              ...form,
-              answer: { ...form.answer, value: event.target.value },
-            })
-          }
-        />
-        <TextField
-          label="Units (if applicable)"
-          fullWidth
-          multiline
-          value={form.answer.units}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            setForm({
-              ...form,
-              answer: { ...form.answer, units: event.target.value },
-            })
-          }
-        />
-      </Stack>
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        sx={{ float: "right", mt: 2 }}
-      >
-        Submit
-      </Button>
-    </>
+      <InputContainer>
+        <Typography variant="h5">Answer</Typography>
+        <Stack direction="row" spacing={2}>
+          <TextField
+            label="Answer"
+            fullWidth
+            multiline
+            required
+            value={form.answer.value}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setForm({
+                ...form,
+                answer: { ...form.answer, value: event.target.value },
+              })
+            }
+          />
+          <TextField
+            label="Units (if applicable)"
+            fullWidth
+            multiline
+            value={form.answer.units}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setForm({
+                ...form,
+                answer: { ...form.answer, units: event.target.value },
+              })
+            }
+          />
+        </Stack>
+      </InputContainer>
+      <Box>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{ float: "right" }}
+        >
+          Submit
+        </Button>
+      </Box>
+    </Stack>
   );
 };
 
