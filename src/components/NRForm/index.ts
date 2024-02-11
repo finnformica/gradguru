10c2ecfe79d4a8cb0b312f38c58ed42bddc3a3cf
finnformica <finnformica@gmail.com>
2024-02-11 16:36:29 +0000

    refactor file name

--- src/components/NRForm/index.ts
+++ src/components/NRForm/index.ts
@@ -1,5 +1,5 @@
 import { EditableTable, FullFeaturedCRUDTable } from "./EditableTable";
-import { TableQuestionElement } from "./TableQuestionElement";
+import { TableQuestionElement } from "./QuestionElement";
 import TableForm from "./TableForm";
 import GraphForm from "./GraphForm";
 import NRForm from "./NRForm";
