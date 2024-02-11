253b8c03e2454318f034afad6142ad58f8856007
finnformica <finnformica@gmail.com>
2024-02-11 19:50:31 +0000

    create documents in firestore nr-consulting collection

--- src/app/(admin)/admin/tests/numerical-reasoning/add/page.tsx
+++ src/app/(admin)/admin/tests/numerical-reasoning/add/page.tsx
@@ -3,10 +3,13 @@
 import { useState } from "react";
 import { Typography } from "@mui/material";
 
+import { useAlert } from "@/context/adminAlert";
+
 import { NRForm } from "@/components/NRForm";
 import { INRForm, tableForm } from "@/components/NRForm/types";
 
 const AddNR = () => {
+  const { setAlertState } = useAlert();
   const [form, setForm] = useState<INRForm>({
     ...tableForm,
   });
@@ -15,6 +18,32 @@ const AddNR = () => {
     e.preventDefault();
 
     console.log("form submitted", form);
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/firebase/document?collection=nr-consulting`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      }
+    );
+
+    if (response.status === 200) {
+      setAlertState({
+        message: "Numerical reasoning question added",
+        severity: "success",
+        open: true,
+      });
+      setForm({ ...tableForm });
+    } else {
+      setAlertState({
+        message: "Uh oh! Error occurred :(",
+        severity: "error",
+        open: true,
+      });
+    }
   };
 
   return (
