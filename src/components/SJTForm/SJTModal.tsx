d3f11e5b910b9643dda450f846b8fec24f7f10b1
finnformica <finnformica@gmail.com>
2024-02-20 20:37:17 +0000

    refactor code to create form modal wrapper

--- src/components/SJTForm/SJTModal.tsx
+++ src/components/SJTForm/SJTModal.tsx
@@ -1,36 +1,12 @@
 "use client";
 
-import CloseIcon from "@mui/icons-material/Close";
-import {
-  Box,
-  Button,
-  IconButton,
-  Modal,
-  Paper,
-  Popper,
-  Stack,
-  Typography,
-} from "@mui/material";
 import { useState } from "react";
 
 import { useAlert } from "@/context/adminAlert";
 
+import FormModalWrapper from "../Global/FormModalWrapper";
 import SJTForm from "./SJTForm";
 import { SJTQuestion } from "./types";
-
-const style = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "90vw",
-  height: "90vh",
-  bgcolor: "background.paper",
-  borderRadius: 2,
-  boxShadow: 24,
-  p: 4,
-  overflow: "auto",
-};
 
 const SJTModal = ({
   open,
@@ -47,9 +23,6 @@ const SJTModal = ({
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(anchorEl ? null : event.currentTarget);
   };
-
-  const openPopper = Boolean(anchorEl);
-  const id = open ? "simple-popper" : undefined;
 
   const handleClose = () => {
     setAnchorEl(null);
@@ -111,62 +84,16 @@ const SJTModal = ({
   };
 
   return (
-    <Modal open={open} onClose={() => setOpen(false)}>
-      <Box sx={style}>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            pb: 3,
-          }}
-        >
-          <Stack direction="row" spacing={4}>
-            <Typography variant="h4">Edit SJT question</Typography>
-            <Button
-              variant="contained"
-              color="error"
-              size="small"
-              onClick={handleClick}
-            >
-              Delete
-            </Button>
-            <Popper
-              id={id}
-              open={openPopper}
-              anchorEl={anchorEl}
-              sx={{ zIndex: (theme) => theme.zIndex.modal + 1 }}
-            >
-              <Paper sx={{ p: 1, borderRadius: 1 }}>
-                <Typography pb={2} variant="h6">
-                  Are you sure?
-                </Typography>
-                <Stack direction="row" spacing={2}>
-                  <Button
-                    variant="contained"
-                    color={"error"}
-                    onClick={handleDelete}
-                  >
-                    Confirm
-                  </Button>
-                  <Button
-                    onClick={handleClick}
-                    variant="outlined"
-                    color={"error"}
-                  >
-                    Cancel
-                  </Button>
-                </Stack>
-              </Paper>
-            </Popper>
-          </Stack>
-          <IconButton onClick={handleClose}>
-            <CloseIcon />
-          </IconButton>
-        </Box>
-        <SJTForm form={form} setForm={setForm} handleSubmit={handleSubmit} />
-      </Box>
-    </Modal>
+    <FormModalWrapper
+      open={open}
+      anchorEl={anchorEl}
+      onClose={handleClose}
+      handleDelete={handleDelete}
+      handleClick={handleClick}
+      handleClose={handleClose}
+    >
+      <SJTForm form={form} setForm={setForm} handleSubmit={handleSubmit} />
+    </FormModalWrapper>
   );
 };
 
