bceaaebf1681db4e62f3d69535416bf5ac2312e4
finnformica <finnformica@gmail.com>
2024-02-24 21:40:32 +0000

    refactor auth config

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,6 @@
-import { FirestoreAdapter } from "@auth/firebase-adapter";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import type { Adapter } from "next-auth/adapters";
+import NextAuth from "next-auth";
 
-import { db } from "@/firebase/config";
-import GoogleProvider from "next-auth/providers/google";
-import * as firestoreFunctions from "firebase/firestore";
-
-const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId:
-        "836524878091-rge0o0k865mljuh44kubr6g0dtls1mh2.apps.googleusercontent.com",
-      clientSecret: "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",
-    }),
-  ],
-  adapter: FirestoreAdapter({
-    db: db,
-    ...firestoreFunctions,
-  }) as Adapter,
-};
+import { authOptions } from "@/auth/config";
 
 const handler = (req: any, res: any) => NextAuth(req, res, authOptions);
 
---
+++ src/auth/config.ts
@@ -0,0 +1,41 @@
+import { cert } from "firebase-admin/app";
+
+import { NextAuthOptions } from "next-auth";
+
+import GoogleProvider from "next-auth/providers/google";
+
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import type { Adapter } from "next-auth/adapters";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+  ],
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    }),
+  }) as Adapter,
+  callbacks: {
+    async jwt({ token, user }) {
+      console.log("jwt", user);
+      // if (user?.id) {
+      //   token.id = user.id;
+      // }
+      // if (user?.userName) {
+      //   token.userName = user.userName;
+      // }
+      return token;
+    },
+    async session({ session, token }) {
+      // session.id = token.id;
+      // session.userName = token.userName;
+      return session;
+    },
+  },
+};
