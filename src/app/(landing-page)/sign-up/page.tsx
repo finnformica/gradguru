716eec9f56e65d707442e54bcb8c0581599a3c46
finnformica <finnformica@gmail.com>
2024-02-25 16:59:22 +0000

    create sign up page

--- src/app/(landing-page)/sign-up/page.tsx
+++ src/app/(landing-page)/sign-up/page.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+"use client";
+
+import { useSession } from "next-auth/react";
+
+import AuthForm from "@/components/LandingPage/AuthForm/AuthForm";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const session = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session.status === "authenticated") {
+      router.push("/dashboard");
+    }
+  });
+
+  return (
+    <AuthForm
+      title="Create an account"
+      subtitle="Already have an account?"
+      method="sign-in"
+      email={email}
+      password={password}
+      setEmail={(e) => setEmail(e.target.value)}
+      setPassword={(e) => setPassword(e.target.value)}
+      handleSubmit={() => console.log("submit")}
+    />
+  );
 };
 
 export default SignUp;
