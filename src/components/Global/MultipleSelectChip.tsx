f0a8d85482f79c74d8ee8087474794f3ea8892e5
finnformica <finnformica@gmail.com>
2024-02-27 22:17:28 +0000

    create multiple select chip component

---
+++ src/components/Global/MultipleSelectChip.tsx
@@ -0,0 +1,63 @@
+import { SxProps } from "@mui/material";
+import Box from "@mui/material/Box";
+import Chip from "@mui/material/Chip";
+import MenuItem from "@mui/material/MenuItem";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { useState } from "react";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+export default function MultipleSelectChip({
+  options,
+  values,
+  sx,
+}: {
+  options: string[];
+  values: string[];
+  sx?: SxProps;
+}) {
+  const [option, setOption] = useState<string[]>(values);
+
+  const handleChange = (event: SelectChangeEvent<typeof option>) => {
+    const {
+      target: { value },
+    } = event;
+    setOption(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
+
+  return (
+    <Select
+      multiple
+      value={option}
+      onChange={handleChange}
+      sx={{ width: "100%", ...sx }}
+      input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+      renderValue={(selected) => (
+        <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+          {selected.map((value) => (
+            <Chip key={value} label={value} />
+          ))}
+        </Box>
+      )}
+    >
+      {options.map((option) => (
+        <MenuItem key={option} value={option}>
+          {option}
+        </MenuItem>
+      ))}
+    </Select>
+  );
+}
