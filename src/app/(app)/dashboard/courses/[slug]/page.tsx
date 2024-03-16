ba0ca556da80eeb1f0d8e518de10f82dfc4ec237
finnformica <finnformica@gmail.com>
2024-03-16 15:44:40 +0000

    refactor course video page route

--- src/app/(app)/dashboard/courses/[slug]/page.tsx
+++ src/app/(app)/dashboard/courses/[slug]/page.tsx
@@ -1,49 +1,14 @@
-"use client";
+import { PageBreadcrumbs } from "components/global-components";
 
-import { notFound } from "next/navigation";
-
-import { Box, Container } from "@mui/material";
-
-import { useCourse } from "api/courses";
-import {
-  CourseDescription,
-  VideoControls,
-  VideoPlayer,
-} from "components/CourseVideoPage";
-import { LoadingScreen } from "components/global-components";
-import { useCourse as useCourseContext } from "context/course";
-import { useSession } from "next-auth/react";
-
-type CoursePageProps = {
-  params: { slug: string };
-};
-
-const CoursePage = ({ params }: CoursePageProps) => {
-  const { data: session } = useSession();
-  const { setCourse } = useCourseContext();
+const CourseInfoPage = ({ params }: { params: { slug: string } }) => {
   const { slug } = params;
 
-  const { course, loading, error } = useCourse(slug);
-
-  // if data has loaded and course is null, or if user does not have access, return 404
-  if ((!loading && !course) || error || !session?.user.courses.includes(slug)) {
-    notFound();
-  }
-
-  // if data is loading, show loading screen
-  if (!course || loading) return <LoadingScreen />;
-
-  setCourse(course);
-
   return (
-    <Container maxWidth="lg" disableGutters>
-      <Box>
-        <VideoPlayer />
-        <VideoControls />
-      </Box>
-      <CourseDescription />
-    </Container>
+    <PageBreadcrumbs
+      header={slug}
+      links={[{ label: "Dashboard", href: "/dashboard" }, { label: slug }]}
+    />
   );
 };
 
-export default CoursePage;
+export default CourseInfoPage;
---
+++ src/app/(app)/dashboard/courses/[slug]/video/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { notFound } from "next/navigation";
+
+import { Box, Container } from "@mui/material";
+
+import { useCourse } from "api/courses";
+import {
+  CourseDescription,
+  VideoControls,
+  VideoPlayer,
+} from "components/CourseVideoPage";
+import { LoadingScreen } from "components/global-components";
+import { useCourse as useCourseContext } from "context/course";
+import { useSession } from "next-auth/react";
+
+type CoursePageProps = {
+  params: { slug: string };
+};
+
+const CoursePage = ({ params }: CoursePageProps) => {
+  const { data: session } = useSession();
+  const { setCourse } = useCourseContext();
+  const { slug } = params;
+
+  const { course, loading, error } = useCourse(slug);
+
+  // if data has loaded and course is null, or if user does not have access, return 404
+  if ((!loading && !course) || error || !session?.user.courses.includes(slug)) {
+    notFound();
+  }
+
+  // if data is loading, show loading screen
+  if (!course || loading) return <LoadingScreen />;
+
+  setCourse(course);
+
+  return (
+    <Container maxWidth="lg" disableGutters>
+      <Box>
+        <VideoPlayer />
+        <VideoControls />
+      </Box>
+      <CourseDescription />
+    </Container>
+  );
+};
+
+export default CoursePage;
