67809547e3238278a589fd393d63d972f6008742
finnformica <finnformica@gmail.com>
2024-03-16 17:17:03 +0000

    create dashboard home page

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,51 +1,70 @@
 "use client";
 import { useSession } from "next-auth/react";
-import { notFound } from "next/navigation";
 
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
-import { useCourses } from "api/courses";
-import { CourseAccordion } from "components/Dashboard/CourseAccordion";
-import { LoadingScreen, PageBreadcrumbs } from "components/global-components";
-import { CourseType } from "components/globalTypes";
+import { LoadingScreen } from "components/global-components";
+import Image from "next/image";
+import Link from "next/link";
+
+const WelcomePanel = ({ name, courses }: { name: string; courses: any[] }) => (
+  <Box
+    sx={{
+      backgroundColor: (theme) => theme.palette.primary.light,
+      width: { xs: "100%", lg: "60%" },
+      borderRadius: 10,
+    }}
+  >
+    <Stack
+      direction={{ xs: "column", md: "row" }}
+      justifyContent={{ xs: "center", md: "space-between" }}
+      alignItems={{ xs: "center", md: "normal" }}
+    >
+      <Stack spacing={2} pt={8} pl={{ xs: 0, md: 6 }}>
+        <Typography variant="h5" fontWeight={300} gutterBottom>
+          Welcome back, {name} 👋
+        </Typography>
+
+        {courses.length > 0 ? (
+          <Typography variant="body2" fontWeight={200} gutterBottom>
+            Your courses are available via the side navigation
+          </Typography>
+        ) : (
+          <Typography
+            variant="body2"
+            fontWeight={200}
+            gutterBottom
+            maxWidth="400px"
+          >
+            You have no courses available, please visit the{" "}
+            <Link
+              href="/courses"
+              style={{ cursor: "pointer", textDecoration: "underline" }}
+            >
+              course page
+            </Link>{" "}
+            to enrol in a course.
+          </Typography>
+        )}
+      </Stack>
+      <Image
+        src="/imgs/3d/dashboard.svg"
+        alt="dashboard"
+        width={300}
+        height={300}
+      />
+    </Stack>
+  </Box>
+);
 
 const Dashboard = () => {
   const { data: session } = useSession();
 
-  const { courses, loading, error } = useCourses();
+  if (!session?.user) return <LoadingScreen />;
 
-  if (error) return notFound();
+  const { courses, name } = session.user;
 
-  if (!session?.user || loading || !courses) return <LoadingScreen />;
-
-  const userCourses = courses.filter((course: CourseType) =>
-    session.user.courses.includes(course.id)
-  );
-
-  return (
-    <>
-      <PageBreadcrumbs header="Courses" links={[{ label: "Courses" }]} />
-
-      {!userCourses.length ? (
-        <Typography pt={4}>No courses available</Typography>
-      ) : (
-        <Box
-          sx={{
-            display: "flex",
-            gap: 2,
-            flexWrap: "wrap",
-            justifyContent: "center",
-            alignItems: "center",
-            my: 4,
-          }}
-        >
-          {userCourses.map((course, key) => (
-            <CourseAccordion key={key} {...course} />
-          ))}
-        </Box>
-      )}
-    </>
-  );
+  return <WelcomePanel name={name || ""} courses={courses} />;
 };
 
 export default Dashboard;
