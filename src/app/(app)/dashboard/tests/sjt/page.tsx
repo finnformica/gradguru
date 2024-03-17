9b682abf67eb7fe82c5c6b282308c27a61e1bcbd
finnformica <finnformica@gmail.com>
2024-03-17 18:55:57 +0000

    alert user if some questions are unanswered

--- src/app/(app)/dashboard/tests/sjt/page.tsx
+++ src/app/(app)/dashboard/tests/sjt/page.tsx
@@ -4,9 +4,11 @@ import { useSJTTests } from "api/tests";
 import { LoadingScreen } from "components/global-components";
 import SJTTestCard from "components/tests/sjt/sjt-test-card";
 import TopPanel from "components/tests/sjt/top-panel";
+import { useSnackbar } from "notistack";
 
 const SituationalJudgementTest = () => {
   const { questions: allQuestions } = useSJTTests();
+  const { enqueueSnackbar } = useSnackbar();
 
   // map questions into a flat array
   const questions = allQuestions
@@ -19,11 +21,17 @@ const SituationalJudgementTest = () => {
     )
     .flat();
 
-  const onSubmit = (data: any) => {
-    console.log("data submitted", data);
-  };
-
   if (!questions) return <LoadingScreen />;
+
+  const onSubmit = (data: any) => {
+    console.log("form submitted");
+    console.log("data", data);
+
+    if (Object.keys(data).length !== questions.length) {
+      enqueueSnackbar("Not all questions answered", { variant: "error" });
+      return;
+    }
+  };
 
   return (
     <>
