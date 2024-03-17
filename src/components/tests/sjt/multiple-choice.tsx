f3ea35c5c219f1ee81dc18bfcdafd00319bd959e
finnformica <finnformica@gmail.com>
2024-03-17 18:56:55 +0000

    error if multiple choice answer not selected

--- src/components/tests/sjt/multiple-choice.tsx
+++ src/components/tests/sjt/multiple-choice.tsx
@@ -1,5 +1,7 @@
 import {
+  FormControl,
   FormControlLabel,
+  FormHelperText,
   Radio,
   RadioGroup,
   Stack,
@@ -11,36 +13,43 @@ type MultipleChoiceProps = {
   setAnswer: (newAnswer: string) => void;
   answer: string;
   options: string[];
+  error: boolean;
+  helperText: string | boolean;
 };
 
 const MultipleChoice = ({
   setAnswer,
   answer,
   options,
+  error,
+  helperText,
 }: MultipleChoiceProps) => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
     setAnswer(event.target.value);
 
   return (
-    <RadioGroup onChange={handleChange} value={answer}>
-      {options.map((option, index) => (
-        <Stack
-          key={option}
-          spacing={2}
-          direction="row"
-          alignItems="center"
-          my={1}
-        >
-          <Typography variant="body1">{index + 1}.</Typography>
-          <FormControlLabel
+    <FormControl error={error} variant="standard">
+      <RadioGroup onChange={handleChange} value={answer}>
+        {options.map((option, index) => (
+          <Stack
             key={option}
-            value={option}
-            control={<Radio />}
-            label={option}
-          />
-        </Stack>
-      ))}
-    </RadioGroup>
+            spacing={2}
+            direction="row"
+            alignItems="center"
+            my={1}
+          >
+            <Typography variant="body1">{index + 1}.</Typography>
+            <FormControlLabel
+              key={option}
+              value={index + 1}
+              control={<Radio />}
+              label={option}
+            />
+          </Stack>
+        ))}
+      </RadioGroup>
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
+    </FormControl>
   );
 };
 
