dd4350697ecc306c594561a3e4e13c7dccc9a27b
Saajan Patel <saajanpatel@hotmail.com>
2024-03-27 16:20:10 +0000

    initial react-quill set up

--- src/components/BlogForm/AddBlog.tsx
+++ src/components/BlogForm/AddBlog.tsx
@@ -1,20 +1,57 @@
-import { Input, Stack, TextField } from "@mui/material";
-import React from "react";
+"use client";
+
+import { Box, Container, Input, Stack, TextField } from "@mui/material";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+// import "react-quill/dist/quill.bubble.css";
 
 const AddBlog = () => {
+  const [mdxDoc, setMdxDox] = useState("");
+
   return (
-    <form>
-      <Stack direction={"column"} spacing={2}>
-        <TextField label={"Blog Title"} />
-        <TextField label={"Summary"} />
-        <Input type={"file"} />
-        <ReactQuill theme="snow" />
-      </Stack>
-    </form>
+    <Container maxWidth="md">
+      <form>
+        <Stack direction={"column"} spacing={2} py={6}>
+          <TextField label={"Blog Title"} />
+          <TextField label={"Summary"} />
+          <Input type={"file"} />
+          <Box sx={{ border: "1px solid black", minHeight: 100 }}>
+            <ReactQuill
+              theme="snow"
+              value={mdxDoc}
+              onChange={setMdxDox}
+              modules={modules}
+            />
+          </Box>
+        </Stack>
+      </form>
+    </Container>
   );
+};
+
+const modules = {
+  toolbar: [
+    ["bold", "italic", "underline", "strike"], // toggled buttons
+    ["blockquote", "code-block"],
+    ["link", "image", "video", "formula"],
+
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+    [{ script: "sub" }, { script: "super" }], // superscript/subscript
+    [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+    [{ direction: "rtl" }], // text direction
+
+    [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+
+    ["clean"], // remove formatting button
+  ],
 };
 
 export default AddBlog;
