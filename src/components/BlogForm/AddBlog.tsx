e3dbc4e1633b0d571f45eecc5369ab86b46dae25
Saajan Patel <saajanpatel@hotmail.com>
2024-03-28 21:30:16 +0000

    house keeping before pr

--- src/components/BlogForm/AddBlog.tsx
+++ src/components/BlogForm/AddBlog.tsx
@@ -4,44 +4,21 @@ import {
   Box,
   Button,
   Container,
-  Input,
   MenuItem,
   Stack,
   TextField,
 } from "@mui/material";
-import React, { useState } from "react";
-import { Controller, useForm } from "react-hook-form";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import "react-quill/dist/quill.bubble.css";
-import { useSnackbar } from "notistack";
-import { addFormData } from "./types";
 import { blogStorage, postBlog } from "api/blog";
 import { LoadingScreen } from "components/global-components";
 import { useSession } from "next-auth/react";
-
-const modules = {
-  toolbar: [
-    ["bold", "italic", "underline", "strike"], // toggled buttons
-    ["blockquote", "code-block"],
-    ["link", "image", "video", "formula"],
-
-    [{ header: 1 }, { header: 2 }], // custom button values
-    [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
-    [{ script: "sub" }, { script: "super" }], // superscript/subscript
-    [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
-    [{ direction: "rtl" }], // text direction
-
-    [{ size: ["small", false, "large", "huge"] }], // custom dropdown
-    [{ header: [1, 2, 3, 4, 5, 6, false] }],
-
-    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-    [{ font: [] }],
-    [{ align: [] }],
-
-    ["clean"], // remove formatting button
-  ],
-};
+import { useSnackbar } from "notistack";
+import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.bubble.css";
+import "react-quill/dist/quill.snow.css";
+import { addFormData } from "./types";
+import { modules } from "./modulesRQ";
 
 const tagOptions = ["Finance", "Jobs", "Education"];
 
@@ -190,11 +167,16 @@ const AddBlog = () => {
               theme="snow"
               value={content}
               onChange={(newValue) => setContent(newValue)}
-              // modules={modules}
+              modules={modules}
             />
           </Box>
-          <Button size="small" type="submit">
-            Add
+          <Button
+            // size="small"
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            submit
           </Button>
         </Stack>
       </form>
--- src/components/BlogForm/types.ts
+++ src/components/BlogForm/types.ts
@@ -1,11 +1,3 @@
-export type DataProps = {
-  title: string;
-  desc: string;
-  tags: string;
-};
-
-export type titleDataProps = { title: string };
-
 export type addFormData = {
   title: string;
   summary: string;
