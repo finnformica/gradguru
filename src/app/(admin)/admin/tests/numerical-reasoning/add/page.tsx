f715cd21d76db7379a32456538f20f1e38a8a176
finnformica <finnformica@gmail.com>
2024-03-30 16:04:16 +0000

    add pages to create and view nr tests

---
+++ src/app/(admin)/admin/tests/numerical-reasoning/add/page.tsx
@@ -0,0 +1,179 @@
+"use client";
+
+import _ from "lodash";
+import { useSnackbar } from "notistack";
+import { Controller, useForm } from "react-hook-form";
+
+import {
+  Autocomplete,
+  Button,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+import { createTest, getQuestions, patchQuestion } from "api/tests";
+import { LoadingScreen } from "components/global-components";
+import { useEffect, useState } from "react";
+
+const NR_GRAPH_QUESTIONS_PER_TEST = 1;
+const NR_TABLE_QUESTIONS_PER_TEST = 1;
+const NR_GMAT_QUESTIONS_PER_TEST = 2;
+const NR_QUESTIONS_PER_TEST =
+  NR_GMAT_QUESTIONS_PER_TEST +
+  NR_GRAPH_QUESTIONS_PER_TEST +
+  NR_TABLE_QUESTIONS_PER_TEST;
+
+type renderInputProps = {
+  title: string;
+  options: any[];
+  numQuestions: number;
+  control: any;
+  setValue: (index: string, id: string) => void;
+};
+
+const renderInput = ({
+  title,
+  options,
+  numQuestions,
+  control,
+  setValue,
+}: renderInputProps) => (
+  <>
+    <Typography variant="h5" pb={2}>
+      {title}
+    </Typography>
+    <Stack spacing={2} pb={2}>
+      {_.range(numQuestions).map((i) => (
+        <Controller
+          key={i}
+          name={`${title.toLowerCase()}.${i}`}
+          control={control}
+          rules={{ required: true }}
+          render={({ field: { value }, fieldState: { error } }) => (
+            <Autocomplete
+              fullWidth
+              value={value}
+              options={options}
+              onChange={(_, data) =>
+                setValue(`${title.toLowerCase()}.${i}`, data.id)
+              }
+              isOptionEqualToValue={(option, value) => option.id === value.id}
+              getOptionKey={(option) => option.id}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label={`${title} Question ${i + 1}`}
+                  error={!!error}
+                  helperText={!!error && "Question is required"}
+                />
+              )}
+            />
+          )}
+        />
+      ))}
+    </Stack>
+  </>
+);
+
+const AddNRTest = () => {
+  const { enqueueSnackbar } = useSnackbar();
+  const [questions, setQuestions] = useState<any[]>([]);
+  const { control, handleSubmit, setValue, reset } = useForm();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getQuestions("numerical-reasoning");
+      setQuestions(res);
+    };
+
+    fetchData();
+  }, []);
+
+  const onSubmit = async (data: any) => {
+    const uniqueQuestions = _.uniq(Object.values(data).flat()) as string[];
+
+    if (uniqueQuestions.length < NR_QUESTIONS_PER_TEST) {
+      enqueueSnackbar(
+        "Duplicatations detected, please select unique questions.",
+        { variant: "error" }
+      );
+      return;
+    }
+
+    createTest("numerical-reasoning", { questions: data })
+      .then((id) => {
+        // add testId to each question
+        uniqueQuestions.forEach(
+          (question) =>
+            patchQuestion(question, "numerical-reasoning", { testId: null }) // TODO: use id
+        );
+      })
+      .then(() => enqueueSnackbar("Test created successfully"))
+      .catch((error) =>
+        enqueueSnackbar(`Error creating test: ${error}`, { variant: "error" })
+      )
+      .finally(() => reset()); // TODO: autcomplete label not resetting
+  };
+
+  if (!questions) return <LoadingScreen />;
+
+  const available = questions.filter((question) => !question.testId);
+
+  const gmatOptions = available
+    .filter((q) => q.type === "gmat")
+    .map((question) => ({
+      id: question.id,
+      label: question.question || "No question",
+    }));
+
+  const graphOptions = available
+    .filter((q) => q.type === "graph")
+    .map((question) => ({
+      id: question.id,
+      label: question.scenario || "No scenario",
+    }));
+
+  const tableOptions = available
+    .filter((q) => q.type === "table")
+    .map((question) => ({
+      id: question.id,
+      label: question.questions[0].question,
+    }));
+
+  return (
+    <>
+      <Typography variant="h4" pb={2}>
+        Create Numerical Reasoning Test
+      </Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {renderInput({
+          title: "Table",
+          options: tableOptions,
+          numQuestions: NR_TABLE_QUESTIONS_PER_TEST,
+          control,
+          setValue,
+        })}
+        {renderInput({
+          title: "Graph",
+          options: graphOptions,
+          numQuestions: NR_GRAPH_QUESTIONS_PER_TEST,
+          control,
+          setValue,
+        })}
+        {renderInput({
+          title: "GMAT",
+          options: gmatOptions,
+          numQuestions: NR_GMAT_QUESTIONS_PER_TEST,
+          control,
+          setValue,
+        })}
+        <Button variant="contained" type="submit">
+          Create
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default AddNRTest;
---
+++ src/app/(admin)/admin/tests/numerical-reasoning/all/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useSnackbar } from "notistack";
+import { useEffect, useState } from "react";
+
+import { Chip, Typography } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+import { deleteTest, getTests, patchQuestion } from "api/tests";
+import EditDeleteActions from "components/data-grid/edit-delete-action";
+import { ConfirmationDialog } from "components/global-components";
+
+const AllNRTests = () => {
+  const { data: session } = useSession();
+  const { enqueueSnackbar } = useSnackbar();
+  const [tests, setTests] = useState<any[]>([]);
+  const [testToDelete, setTestToDelete] = useState<any | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getTests("numerical-reasoning");
+      setTests(res);
+    };
+
+    fetchData();
+  }, []);
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 200 },
+    {
+      field: "questions",
+      headerName: "Questions",
+      flex: 1,
+      width: 200,
+      renderCell: (params) =>
+        (Object.values(params.value).flat() as string[]).map((q) => (
+          <Chip key={q} label={q} sx={{ mx: 0.5 }} />
+        )),
+    },
+    {
+      field: "created",
+      headerName: "Created",
+      width: 200,
+      renderCell: (params) => new Date(params.value).toLocaleString(),
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 100,
+      renderCell: (params) => (
+        <EditDeleteActions
+          session={session}
+          onDeleteClick={() => setTestToDelete(params.row)}
+        />
+      ),
+    },
+  ];
+
+  const handleDeleteTest = async () => {
+    console.log("Deleting test", testToDelete.id);
+
+    const flattenedQuestions = Object.values(
+      testToDelete.questions
+    ).flat() as string[];
+
+    // delete reference to testId from each question
+    const questionsUpdated = await flattenedQuestions.forEach((question) =>
+      patchQuestion(question, "numerical-reasoning", { testId: null })
+    );
+
+    // delete the test
+    const testDeleted = await deleteTest(
+      "numerical-reasoning",
+      testToDelete.id
+    );
+
+    // wait for all promises to resolve
+    Promise.all([questionsUpdated, testDeleted])
+      .then(() => enqueueSnackbar("Test deleted"))
+      .catch(() => enqueueSnackbar("Error deleting test"))
+      .finally(() => setTestToDelete(null));
+  };
+
+  return (
+    <>
+      <Typography variant="h4" pb={4}>
+        All Numerical Reasoning Tests
+      </Typography>
+      <DataGrid
+        rows={tests}
+        columns={columns}
+        autoHeight
+        sx={{ minHeight: 400 }}
+      />
+      {testToDelete && (
+        <ConfirmationDialog
+          open={!!testToDelete}
+          onClose={() => setTestToDelete(null)}
+          onSubmit={handleDeleteTest}
+          title="Are you sure you want to delete this test?"
+          confirmText="Delete"
+        />
+      )}
+    </>
+  );
+};
+
+export default AllNRTests;
