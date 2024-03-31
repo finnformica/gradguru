08b96eec865140096d1d4da7c2bb0cf965057c33
finnformica <finnformica@gmail.com>
2024-03-31 18:36:25 +0100

    delete old code

--- src/app/(app)/dashboard/tests/page.tsx
+++ src/app/(app)/dashboard/tests/page.tsx
@@ -1,52 +1,11 @@
 "use client";
 
 import _ from "lodash";
-
-import { Card, CardActionArea, CardHeader, Grid, Rating } from "@mui/material";
-
-import { GridColDef } from "@mui/x-data-grid";
 import { useRouter } from "next/navigation";
+
+import { Card, CardActionArea, CardHeader, Grid } from "@mui/material";
+
 import TopPanel from "./top-panel";
-
-// keep for test pages
-const columns: GridColDef[] = [
-  { field: "type", headerName: "Type", width: 250 },
-  {
-    field: "score",
-    headerName: "Score",
-    width: 150,
-    renderCell: (params) => `${(params.value * 100).toFixed(2)}%`,
-  },
-  {
-    field: "id",
-    headerName: "Date",
-    width: 200,
-    renderCell: (params) => new Date(params.value).toLocaleString(),
-  },
-  {
-    field: "rating",
-    headerName: "Rating",
-    width: 150,
-    renderCell: (params) => (
-      <Rating value={Math.ceil(params.value * 5)} readOnly />
-    ),
-  },
-  {
-    field: "time",
-    headerName: "Time taken",
-    width: 150,
-    renderCell: (params) => {
-      const date = new Date(params.value);
-      const hours = date.getUTCHours();
-      const minutes = date.getUTCMinutes();
-      const seconds = date.getUTCSeconds();
-
-      return params.value
-        ? `${hours ? `${hours}h ` : ""}${minutes ? `${minutes}m ` : ""}${seconds ? `${seconds}s` : ""}`
-        : null;
-    },
-  },
-];
 
 const CourseTests = () => {
   const router = useRouter();
