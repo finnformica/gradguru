81333c77bffab2ae3371a655d147228365010a85
finnformica <finnformica@gmail.com>
2024-04-01 22:04:40 +0100

    refactor triangle element

--- src/components/aptitude-tests/logical-reasoning/triangle-grid.tsx
+++ src/components/aptitude-tests/logical-reasoning/triangle-grid.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import _ from "lodash";
 
 import { Box, Stack } from "@mui/material";
@@ -9,6 +11,53 @@ import { Box, Stack } from "@mui/material";
 const NUM_ROWS = 4;
 const TRIANGLE_SIZE = "60px";
 
+const TriangleCell = ({
+  children,
+  index,
+  onClick,
+}: {
+  children: React.ReactNode;
+  index: number;
+  onClick?: any;
+}) => (
+  <Box
+    onClick={onClick}
+    sx={{
+      display: "grid",
+      placeItems: "center",
+      width: TRIANGLE_SIZE,
+      height: TRIANGLE_SIZE,
+      margin: `0 calc(-1 * ${TRIANGLE_SIZE} / 4)`,
+      backgroundColor: "black",
+      transition: "background-color 0.3s ease-out",
+      cursor: "pointer",
+      clipPath:
+        index % 2 === 0
+          ? "polygon(50%   0%, 0% 100%, 100% 100%)"
+          : "polygon(50% 100%, 0%   0%, 100%   0%)",
+      "&:hover:before": {
+        backgroundColor: "#DFDFDF",
+      },
+
+      "&:before": {
+        content: '""',
+        width: `calc(${TRIANGLE_SIZE} * 0.98)`,
+        height: `calc(${TRIANGLE_SIZE} * 0.98)`,
+        position: "absolute",
+        backgroundColor: "white",
+        transition: "background-color 0.3s ease-out",
+        zIndex: -1,
+        clipPath:
+          index % 2 === 0
+            ? "polygon(50%   0%, 0% 100%, 100% 100%)"
+            : "polygon(50% 100%, 0%   0%, 100%   0%)",
+      },
+    }}
+  >
+    {children}
+  </Box>
+);
+
 const TriangleGrid = () => {
   const rows = _.range(1, NUM_ROWS * 2, 2);
 
@@ -16,43 +65,14 @@ const TriangleGrid = () => {
     <Stack direction="column">
       {rows.map((row) => (
         <Box key={row} display="flex" justifyContent="center">
-          {_.range(row).map((cell, i) => (
-            <Box
-              key={cell}
-              sx={{
-                display: "grid",
-                placeItems: "center",
-                width: TRIANGLE_SIZE,
-                height: TRIANGLE_SIZE,
-                margin: `0 calc(-1 * ${TRIANGLE_SIZE} / 4)`,
-                backgroundColor: "black",
-                transition: "background-color 0.3s ease-out",
-                cursor: "pointer",
-                clipPath:
-                  i % 2 === 0
-                    ? "polygon(50%   0%, 0% 100%, 100% 100%)"
-                    : "polygon(50% 100%, 0%   0%, 100%   0%)",
-                "&:hover:before": {
-                  backgroundColor: "#DFDFDF",
-                },
-
-                "&:before": {
-                  content: '""',
-                  width: `calc(${TRIANGLE_SIZE} * 0.98)`,
-                  height: `calc(${TRIANGLE_SIZE} * 0.98)`,
-                  position: "absolute",
-                  backgroundColor: "white",
-                  transition: "background-color 0.3s ease-out",
-                  zIndex: -1,
-                  clipPath:
-                    i % 2 === 0
-                      ? "polygon(50%   0%, 0% 100%, 100% 100%)"
-                      : "polygon(50% 100%, 0%   0%, 100%   0%)",
-                },
-              }}
+          {_.range(row).map((col, i) => (
+            <TriangleCell
+              key={i}
+              index={i}
+              onClick={() => console.log("clicked", row, col)}
             >
-              {cell}
-            </Box>
+              {col}
+            </TriangleCell>
           ))}
         </Box>
       ))}
