abaec522bd4153fd2e88239b2b3dd7144ae5537a
Saajan Patel <saajanpatel@hotmail.com>
2024-04-11 18:58:18 +0100

    basic edit modal

--- src/app/(admin)/admin/blog/all/page.tsx
+++ src/app/(admin)/admin/blog/all/page.tsx
@@ -2,6 +2,7 @@
 import { Container, Typography } from "@mui/material";
 import { GridColDef } from "@mui/x-data-grid";
 import { getBlogs } from "api/blog";
+import EditModal from "components/blog/EditModal";
 import { AdminDataGrid, EditDeleteActions } from "components/data-grid-custom";
 import {
   ConfirmationDialog,
@@ -10,7 +11,6 @@ import {
 import { deleteDoc, doc } from "firebase/firestore";
 import { db } from "lib/firebase/config";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import { enqueueSnackbar } from "notistack";
 import { useEffect, useState } from "react";
 import { IBlogPage } from "types/blog";
@@ -18,13 +18,12 @@ import { IBlogPage } from "types/blog";
 const BlogEditTable = () => {
   const [blogToDelete, setBlogToDelete] = useState<IBlogPage | null>(null);
   const [blogs, setBlogs] = useState<IBlogPage[]>([]);
-  const router = useRouter();
+  const [editBlog, setEditBlog] = useState<IBlogPage | null>(null);
   const { data: session } = useSession();
 
   useEffect(() => {
     // add event listener on firestore collectionxX
     const unsubscribe = getBlogs(setBlogs);
-
     // remove event listener on unmount
     return () => unsubscribe();
   }, []);
@@ -51,7 +50,7 @@ const BlogEditTable = () => {
         <EditDeleteActions
           session={session}
           onDeleteClick={() => setBlogToDelete(params.row)}
-          onEditClick={() => router.refresh}
+          onEditClick={() => setEditBlog(params.row)}
         />
       ),
     },
@@ -70,6 +69,8 @@ const BlogEditTable = () => {
       });
   };
 
+  const handleEditBlog = () => console.log("blog edited");
+
   if (!session) return <LoadingScreen />;
 
   if (!blogs) return <LoadingScreen />;
@@ -85,6 +86,14 @@ const BlogEditTable = () => {
           onSubmit={handleDeleteblog}
           title="Are you sure you want to delete this blog post?"
           confirmText="Delete"
+        />
+      )}
+      {editBlog && (
+        <EditModal
+          slug={editBlog.slug}
+          onClose={() => setEditBlog(null)}
+          onSubmit={handleEditBlog}
+          open={!!editBlog}
         />
       )}
     </Container>
