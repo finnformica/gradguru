0a52940a0e1a7ac49a5191ed3b5c881fb4d2f001
finnformica <finnformica@gmail.com>
2024-04-16 20:21:40 +0100

    use iconify for extra icons

--- src/components/global-components/index.ts
+++ src/components/global-components/index.ts
@@ -2,12 +2,14 @@ import ClientWrapper from "./ClientWrapper";
 import LoadingWrapper from "./LoadingWrapper";
 import NotFoundComponent from "./NotFoundComponent";
 import ConfirmationDialog from "./confirmation-dialog";
+import Iconify from "./iconify";
 import LoadingScreen from "./loading-screen";
 import PageBreadcrumbs from "./page-breadcrumbs";
 
 export {
   ClientWrapper,
   ConfirmationDialog,
+  Iconify,
   LoadingScreen,
   LoadingWrapper,
   NotFoundComponent,
