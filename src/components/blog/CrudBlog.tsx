b0f49c5cb9d9368096c5b9793c278f3cbc4a3b4c
Finn Formica <finn.formica@carbonuw.com>
2024-04-26 22:43:13 +0100

    remove loading screens so content can be loaded offline

--- src/components/blog/CrudBlog.tsx
+++ src/components/blog/CrudBlog.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useSession } from "next-auth/react";
 import { useSnackbar } from "notistack";
 import { useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
@@ -21,7 +20,6 @@ import {
 } from "@mui/material";
 
 import { getBlogTags } from "api/blog";
-import { LoadingScreen } from "components/global";
 import { IBlog } from "types/blog";
 
 import AddingHeroImage from "./AddingHeroImage";
@@ -55,7 +53,6 @@ type addBlogProps = {
 
 // import component using next.js dynamic to avoid SSR
 const CrudBlog = ({ onSubmitBlog, defaultValues }: addBlogProps) => {
-  const { data: session } = useSession();
   const { enqueueSnackbar } = useSnackbar();
   const [reviewBlog, setReviewBlog] = useState(false);
   const [options, setOptions] = useState<string[]>([]);
@@ -245,8 +242,6 @@ const CrudBlog = ({ onSubmitBlog, defaultValues }: addBlogProps) => {
       </Stack>
     </Box>
   );
-
-  if (!session) return <LoadingScreen />;
 
   return (
     <Container maxWidth="md">
