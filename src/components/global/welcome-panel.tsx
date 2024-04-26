b4c382fbb66fe93852dde9e272efce6de5c9684e
Finn Formica <finn.formica@carbonuw.com>
2024-04-26 22:31:27 +0100

    refactor welcome panel into global component

---
+++ src/components/global/welcome-panel.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Image from "next/image";
+
+import { Box, Stack, Typography } from "@mui/material";
+
+export const WelcomePanel = ({
+  name,
+  children,
+}: {
+  name: string;
+  children?: React.ReactNode;
+}) => (
+  <Box
+    sx={{
+      backgroundColor: (theme) => theme.palette.primary.light,
+      width: { xs: "100%", lg: "60%" },
+      borderRadius: 10,
+    }}
+  >
+    <Stack
+      direction={{ xs: "column", md: "row" }}
+      justifyContent={{ xs: "center", md: "space-between" }}
+      alignItems={{ xs: "center", md: "normal" }}
+    >
+      <Stack spacing={2} pt={8} pl={{ xs: 0, md: 6 }}>
+        <Typography variant="h5" fontWeight={300} gutterBottom>
+          Welcome back, {name} 👋
+        </Typography>
+
+        {children}
+      </Stack>
+      <Image
+        src="/imgs/3d/dashboard.svg"
+        alt="dashboard"
+        width={300}
+        height={300}
+      />
+    </Stack>
+  </Box>
+);
