549599d41e948b325288979b70ec3ebc3aaaddc8
Finn Formica <finn.formica@carbonuw.com>
2024-05-04 09:10:58 +0100

    util functions to interact with firebase

--- src/components/drills/hirevue/index.ts
+++ src/components/drills/hirevue/index.ts
@@ -1,4 +1,9 @@
 import HirevueQuestionEditModal from "./hirevue-question-edit-modal";
 import HirevueQuestionAdminForm from "./hirevue-question-admin-form";
+import AddHirevueTypeModal from "./add-hirevue-type-modal";
 
-export { HirevueQuestionAdminForm, HirevueQuestionEditModal };
+export {
+  HirevueQuestionAdminForm,
+  HirevueQuestionEditModal,
+  AddHirevueTypeModal,
+};
