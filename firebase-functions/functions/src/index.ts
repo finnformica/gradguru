84c03eded354c8bcf19b591a552e2c42f660c6c1
finnformica <finnformica@gmail.com>
2024-05-12 11:25:15 +0100

    remove console.log

--- firebase-functions/functions/src/index.ts
+++ firebase-functions/functions/src/index.ts
@@ -10,13 +10,11 @@ const europeFunctions = functions.region("europe-west2");
 
 // delete the user document in the user-meta collection
 exports.deleteUserObject = europeFunctions.auth.user().onDelete((user) => {
-  console.log("Deleting user object", user.uid);
   return firestore().collection("user-meta").doc(user.uid).delete();
 });
 
 // create the user document in the user-meta collection
 exports.createUserObject = europeFunctions.auth.user().onCreate((user) => {
-  console.log("Creating user object", user.uid);
   return firestore().collection("user-meta").doc(user.uid).set({
     id: user.uid,
     email: user.email,
