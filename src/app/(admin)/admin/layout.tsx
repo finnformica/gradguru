05bb227af3eeb0896f25f1a8959fbf554641dc05
finnformica <finnformica@gmail.com>
2024-05-20 20:47:58 +0100

    chore: remove loading screen from admin layout

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
 import { notFound } from "next/navigation";
+import { useEffect } from "react";
 
 import { useUserMeta } from "api/user";
 import { useSession } from "context/user";
 import AdminLayout from "layouts/admin";
-import { LoadingScreen } from "components/global";
 
 export default function AdminLayoutPage({
   children,
@@ -18,13 +17,12 @@ export default function AdminLayoutPage({
 
   useEffect(() => setUser(user), [user, setUser]);
 
-  if (!user) return <LoadingScreen />;
-
   // if user is not authenticated,
   // or does not have update, delete, or create permissions
-  const notAdmin = user.role < 2 || user.role === undefined;
+  const notAdmin = user?.role < 2 || user?.role === undefined;
   if (notAdmin) {
     notFound(); // TODO: nice not found page
   }
+
   return <AdminLayout>{children}</AdminLayout>;
 }
