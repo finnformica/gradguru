980a49d6bb538647e5a9804b4019858d48720164
finnformica <finnformica@gmail.com>
2024-05-21 18:28:11 +0100

    refactor code into single file

--- src/context/snackbar.tsx
+++ src/context/snackbar.tsx
@@ -1,10 +1,25 @@
 "use client";
 
-import { IconButton } from "@mui/material";
-import { Iconify } from "components/global";
-import { iconVariants } from "components/snackbar/variants";
-
+import { IconButton, BoxProps } from "@mui/material";
 import { SnackbarProvider, closeSnackbar } from "notistack";
+
+import { Iconify } from "components/global";
+
+type SnackbarIconProps = {
+  icon: string;
+  colour: BoxProps["color"];
+};
+
+const SnackbarIcon = ({ icon, colour }: SnackbarIconProps) => (
+  <Iconify color={`${colour}.main`} width={25} sx={{ mr: 1.25 }} icon={icon} />
+);
+
+export const iconVariants = {
+  success: <SnackbarIcon icon="ep:success-filled" colour="success" />,
+  error: <SnackbarIcon icon="ic:round-error" colour="error" />,
+  info: <SnackbarIcon icon="ic:round-info" colour="info" />,
+  warning: <SnackbarIcon icon="ic:round-warning" colour="warning" />,
+};
 
 const SnackbarContext = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -17,18 +32,19 @@ const SnackbarContext = ({ children }: { children: React.ReactNode }) => {
       anchorOrigin={{ vertical: "top", horizontal: "right" }}
       style={{
         fontFamily: "Roboto, sans-serif",
-        backgroundColor: "#FFFF",
+        backgroundColor: "#FFFFFF",
         color: "black",
-        boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.3)",
+        boxShadow: "0px 0px 12px rgba(0, 0, 0, 0.15)",
         paddingLeft: "16px",
         paddingRight: "16px",
+        borderRadius: "8px",
       }}
       action={(snackbarId) => (
         <IconButton
           sx={{ color: "grey" }}
           onClick={() => closeSnackbar(snackbarId)}
         >
-          <Iconify icon="fluent:dismiss-12-filled" height="16" />
+          <Iconify icon="fluent:dismiss-12-filled" width={14} />
         </IconButton>
       )}
     >
