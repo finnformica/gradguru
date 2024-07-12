608e74e57ca8b65e6f4cd619bedf28a3140b7c53
Saajan Patel <saajanpatel@hotmail.com>
2024-07-12 15:32:17 +0100

    capture and playback video

--- src/app/(app)/dashboard/practice/page.tsx
+++ src/app/(app)/dashboard/practice/page.tsx
@@ -1,11 +1,58 @@
 "use client";
-import { Button, Container, Skeleton, Stack } from "@mui/material";
+
+import { Button, Container, Stack, Typography } from "@mui/material";
 import { PageBreadcrumbs } from "components/global";
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
+import Webcam from "react-webcam";
 
 const VideoAssessmentPractice = () => {
-  const buttonWidth = "75px";
-  const [recording, setRecording] = useState(false);
+  const webcamRef = useRef<Webcam>(null);
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const [capturing, setCapturing] = useState<boolean>(false);
+  const [recordedChunks, setRecordedChunks] = useState<Blob[]>([]);
+  const [showRecordedVideo, setShowRecordedVideo] = useState<boolean>(false);
+
+  const handleDataAvailable = useCallback(
+    ({ data }: BlobEvent) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => prev.concat(data));
+      }
+    },
+    [setRecordedChunks]
+  );
+
+  const handleStartCaptureClick = useCallback(() => {
+    setCapturing(true);
+    setShowRecordedVideo(false); // Hide recorded video if shown
+
+    if (webcamRef.current && webcamRef.current.stream) {
+      mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+        mimeType: "video/webm",
+      });
+
+      mediaRecorderRef.current.addEventListener(
+        "dataavailable",
+        handleDataAvailable
+      );
+
+      mediaRecorderRef.current.start();
+    }
+  }, [webcamRef, setCapturing, mediaRecorderRef, handleDataAvailable]);
+
+  const handleStopCaptureClick = useCallback(() => {
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+    }
+
+    setCapturing(false);
+    setShowRecordedVideo(true); // Show recorded video
+  }, [mediaRecorderRef, setCapturing, setShowRecordedVideo]);
+
+  const handleReset = useCallback(() => {
+    setRecordedChunks([]);
+    setShowRecordedVideo(false);
+  }, [setRecordedChunks, setShowRecordedVideo]);
+
   return (
     <Container maxWidth={"lg"}>
       <PageBreadcrumbs
@@ -16,41 +63,56 @@ const VideoAssessmentPractice = () => {
         ]}
       />
 
-      <Stack direction={"column"} sx={{ width: "100%", alignItems: "center" }}>
-        <Skeleton
-          variant="rectangular"
-          width={800}
-          height={500}
-          sx={{ my: 10 }}
-        />
+      <Stack
+        direction={"column"}
+        mt={6}
+        gap={6}
+        sx={{ width: "100%", alignItems: "center" }}
+      >
+        <Typography variant="h4" textAlign={"center"}>
+          Question: This is the question that the person will be asnwering
+        </Typography>
         <Stack
           direction={"row"}
-          spacing={10}
+          spacing={2}
           sx={{ width: "100%", justifyContent: "center" }}
         >
-          {recording ? (
+          {capturing ? (
             <Button
-              onClick={() => setRecording(false)}
-              sx={{
-                backgroundColor: "#FF0000",
-                width: buttonWidth,
-                color: "white",
-                "&:hover": { backgroundColor: "rgba(255,0,0,0.8)" },
-              }}
+              onClick={handleStopCaptureClick}
+              variant="contained"
+              color="secondary"
             >
-              Stop
+              Stop Capture
             </Button>
           ) : (
             <Button
-              onClick={() => setRecording(true)}
+              onClick={handleStartCaptureClick}
               variant="contained"
-              sx={{ width: buttonWidth }}
+              color="primary"
             >
-              Record
+              Start Capture
             </Button>
           )}
-          <Button variant="outlined">Reset</Button>
+
+          <Button onClick={handleReset} variant="outlined">
+            Reset
+          </Button>
         </Stack>
+        {showRecordedVideo && recordedChunks.length > 0 ? (
+          <video controls width={800} height={500}>
+            <source
+              src={URL.createObjectURL(
+                new Blob(recordedChunks, { type: "video/webm" })
+              )}
+              type="video/webm"
+            />
+            <track kind="captions" srcLang="en" label="English captions" />
+            Your browser does not support the video tag.
+          </video>
+        ) : (
+          <Webcam audio={false} ref={webcamRef} />
+        )}
       </Stack>
     </Container>
   );
